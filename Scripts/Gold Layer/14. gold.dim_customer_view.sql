1e781787004d31b5ce2d5a42f9d84817e9de8732
StefanoN98 <166292999+StefanoN98@users.noreply.github.com>
2025-03-07 23:16:43 +0100

    Update 14. gold.dim_customer_view.sql

--- Scripts/Gold Layer/14. gold.dim_customer_view.sql
+++ Scripts/Gold Layer/14. gold.dim_customer_view.sql
@@ -1,6 +1,17 @@
-/*===============================================================================
-DDL Script: Create Gold Views -->Create Dimension: gold.dim_customer
-===============================================================================*/
+/*
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+DDL Script: Create Gold Views --> Create Dimension: gold.dim_customer
+===============================================================================
+*/
 
 /*Step 1 : choose the columns I want to keep from silver.crm_cust_info
 		   then assign an alias (ci) */
