d126a8492b918f18f15a37c1a7a587310af6eef1
StefanoN98 <166292999+StefanoN98@users.noreply.github.com>
2025-03-10 13:15:37 +0100

    Create 02 cumulative_analysis.sql

---
+++ 03. ADVANCED ANALYTICS PROJECT/Script/02 cumulative_analysis.sql
@@ -0,0 +1,70 @@
+/*
+===============================================================================
+Cumulative Analysis
+===============================================================================
+Purpose:
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
+
+SQL Functions Used:
+    - Window Functions: SUM() OVER(), AVG() OVER()
+===============================================================================
+*/
+
+
+--1) RUNNING TOTAL - default frame
+	-- Month case
+		WITH cte AS(
+			SELECT DATETRUNC(month, order_date) AS order_date,
+				   SUM(sales_amount) as total_sales
+			FROM gold.fact_sales
+			WHERE order_date IS NOT NULL
+			GROUP BY DATETRUNC(month, order_date)
+			)
+
+			SELECT *,
+				SUM(cte.total_sales) OVER (ORDER BY cte.order_date) AS running_total
+			FROM cte
+
+	-- Year case
+		WITH cte AS(
+			SELECT DATETRUNC(year, order_date) AS order_date,
+				   SUM(sales_amount) as total_sales
+			FROM gold.fact_sales
+			WHERE order_date IS NOT NULL
+			GROUP BY DATETRUNC(year, order_date)
+			)
+
+			SELECT *,
+				SUM(cte.total_sales) OVER (ORDER BY cte.order_date) AS running_total
+			FROM cte
+
+
+--2) RUNNING TOTAL - year frame
+	WITH cte AS(
+		SELECT DATETRUNC(month, order_date) AS order_date,
+			   SUM(sales_amount) as total_sales
+		FROM gold.fact_sales
+		WHERE order_date IS NOT NULL
+		GROUP BY DATETRUNC(month, order_date)
+		)
+
+		SELECT *,
+			SUM(cte.total_sales) OVER (PARTITION BY YEAR(order_date) ORDER BY cte.order_date) AS running_total
+		FROM cte
+
+--3) Add MOVING AVERAGE on the price
+	WITH cte AS(
+			SELECT DATETRUNC(year, order_date) AS order_date,
+				   SUM(sales_amount) AS total_sales,
+				   AVG(price) AS avg_price
+			FROM gold.fact_sales
+			WHERE order_date IS NOT NULL
+			GROUP BY DATETRUNC(year, order_date)
+			)
+
+	SELECT *,
+		SUM(cte.total_sales) OVER (ORDER BY cte.order_date) AS running_total,
+	    AVG(cte.avg_price) OVER (ORDER BY cte.order_date) AS moving_average
+    FROM cte
